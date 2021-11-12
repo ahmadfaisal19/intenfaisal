55c84796506385deee1c031df71500b841235b1f
ahmadfaisal19 <93088680+ahmadfaisal19@users.noreply.github.com>
2021-11-12 23:26:32 +0700

    tugas inten 7
    Ahmad Faisal 20310001

---
+++ app/src/main/java/com/faisal/intenfaisal/MoveForResultActivity.kt
@@ -0,0 +1,39 @@
+package com.faisal.intenfaisal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.google.android.material.snackbar.Snackbar
+
+class MoveForResultActivity : AppCompatActivity() {
+    var RgColor : RadioGroup? = null
+    private lateinit var radioButton: RadioButton
+    private lateinit var btnPilih: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_for_result)
+
+        RgColor = findViewById(R.id.rg_color)
+        btnPilih = findViewById(R.id.btn_choose)
+
+        btnPilih.setOnClickListener {
+
+            val SelectOption: Int = RgColor!!.checkedRadioButtonId
+                radioButton = findViewById(SelectOption)
+                val intent = Intent(this, MainActivity::class.java)
+                val bundle = Bundle()
+                bundle.putString("Warna", radioButton.text as  String?)
+                intent.putExtras(bundle)
+                startActivity(intent)
+
+
+
+        }
+
+    }
+}
